4fa07d8691a50f8c62e87cb425ba89666110e411
Keiji Momose <keiji.momose@soliton.co.jp>
2025-10-06 10:56:30 +0900

    renewVialrepo.shの更新

--- renewVialrepo.sh
+++ renewVialrepo.sh
@@ -1,29 +1,45 @@
 #! /bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "usage: ./renewVialrepo.sh -mg"
+    echo "usage: ./renewVialrepo.sh -amdg"
+    echo "-a -> Update Debian based Distro(apt)"
     echo "-m -> Update MSYS(for Windows Build Environment)"
-    echo "-g -> Update Vial Git Repositry"
+    echo "-d -> Update RHEL based Distro(dnf)"
+    echo "-g -> Update Vial Git Repositry(add official repo as 'upstream' first)"
 
     exit 1
 fi
 
+UPD_APT=FALSE
 UPD_MSYS=FALSE
+UPD_DNF=FALSE
 UPD_GIT=FALSE
 
 OPT=
 OPTIND=
-while getopts gm OPT ; do
+while getopts adgm OPT ; do
     case $OPT in
-        g) UPD_GIT=TRUE; ;;
+        a) UPD_APT=TRUE; ;;
         m) UPD_MSYS=TRUE; ;;
+        d) UPD_DNF=TRUE; ;;
+        g) UPD_GIT=TRUE; ;;
     esac
 done
 shift $(expr $OPTIND - 1)
 
+if [ $UPD_APT = "TRUE" ] ; then
+    #Update for Debian based Distro
+    sudo apt update && sudo apt -y upgrade
+fi
+
 if [ $UPD_MSYS = "TRUE" ] ; then
     #Update for QMK MSYS/MSYS
     pacman -Syu
+fi
+
+if [ $UPD_DNF = "TRUE" ] ; then
+    #Update for RHEL based Distro
+    sudo dnf -y update
 fi
 
 if [ $UPD_GIT = "TRUE" ] ; then

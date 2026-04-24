d6cabd50b10a6de3987ae5c32f600bae28479031
Keiji Momose <keiji.momose@soliton.co.jp>
2026-04-24 11:52:03 +0900

    aki27/cocot46plus:nicola_vial nicola_vialのサンプル実装に揃えた。

--- keyboards/aki27/cocot46plus/keymaps/nicola_vial/keymap.c
+++ keyboards/aki27/cocot46plus/keymaps/nicola_vial/keymap.c
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 // NICOLA親指シフト
-#include "timer.h" // タイマー機能のために追加
 #include "keymap_japanese.h"
 #include "sendstring_japanese.h"
 #include "nicola.h"
@@ -38,11 +37,12 @@ enum layer_number {
     _RAISE,
     _TRACKBALL,
     _L4,
-    _L5
+    _L5,
+    _L6
 };
 
-#define LW_MHEN LT(1,KC_INT5)  // lower
-#define RS_HENK LT(2,KC_INT4)  // raise
+#define LW_MHEN LT(2,KC_INT5)  // lower
+#define RS_HENK LT(3,KC_INT4)  // raise
 #define DEL_ALT ALT_T(KC_DEL)
 
 // In the buttom row, KC_MS_BTN3 binds middle-click to the encoder's button press
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                          KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_MINS,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-                        KC_LALT, KC_LGUI,   LW_MHEN,  KC_SPC,    JP_LBRC,              JP_RBRC,    NCL_ON, KC_BSPC, KC_RGUI, KC_RALT,
+                        KC_LALT, KC_LGUI,   LW_MHEN,  KC_SPC,    MS_BTN1,              MS_BTN2,    NCL_ON, KC_BSPC, KC_RGUI, KC_RALT,
                                                                  XXXXXXX,    SCRL_MO,  XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
@@ -75,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
    TO(_RAISE),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   JP_AT,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      _______, _______, _______, MS_BTN2, MS_BTN1, _______,                                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, JP_COLN, _______,
+      _______, _______, _______, _______, _______, _______,                                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, JP_COLN, _______,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
       _______, _______, _______, _______, _______, _______,                                       KC_HOME,  KC_END, S(KC_TAB), KC_TAB, _______, _______,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-                        _______, _______, _______,  _______,   _______,             _______, _______,  KC_DEL, _______, _______,
+                        _______, _______, _______,  _______,   JP_LBRC,             JP_RBRC, _______,  KC_DEL, _______, _______,
                                                                  XXXXXXX, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
@@ -89,21 +89,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
       _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                                        KC_TAB,   KC_P4,   KC_P5,   KC_P6, KC_PAST, KC_PENT,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      _______, _______, _______, _______,  KC_F11,  KC_F12,                                       _______,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_MINS,
+      _______, _______, _______, _______,  KC_F11,  KC_F12,                                       _______,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, _______,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-                        _______, _______,   TT(4), _______,      _______,           _______,   KC_P0, _______, KC_PDOT,  _______,
+                        _______, _______,LT(4, TO(_BASE)), _______,      _______,           _______,   KC_P0, _______, KC_PDOT,  _______,
                                                                  XXXXXXX, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
   [_TRACKBALL] = LAYOUT(
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,                                       SCRL_TO,  CPI_SW, SCRL_SW, ROT_L15, ROT_R15, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UG_TOGG,                                       SCRL_TO,  CPI_SW, SCRL_SW, ROT_L15, ROT_R15, XXXXXXX,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      XXXXXXX, XXXXXXX, RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,                                       SCRL_MO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, UG_VALU, UG_SATU, UG_HUEU, UG_NEXT,                                       SCRL_MO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      XXXXXXX, XXXXXXX, RGB_VAD, RGB_SAD, RGB_HUD,RGB_RMOD,                                       SCRL_IN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, UG_VALD, UG_SATD, UG_HUED, UG_PREV,                                       SCRL_IN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-                        KC_LGUI, DEL_ALT, KC_TRNS,  KC_SPC,   KC_MS_BTN1,             KC_MS_BTN2,  KC_ENT, RS_HENK, KC_BSPC,  KC_ESC,
+                        _______, _______, _______, _______,      _______,           _______, _______, _______, _______, _______,
                                                                  XXXXXXX, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
@@ -131,7 +131,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                  XXXXXXX, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
-
+   [_L6] = LAYOUT(
+  //|-------------------------------------------------------|                                   |-------------------------------------------------------|
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|-------------------------------------------------------|                                   |-------------------------------------------------------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|-------------------------------------------------------|                                   |-------------------------------------------------------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|-------------------------------------------------------|                                   |-------------------------------------------------------|
+                        _______, _______, _______, _______,      _______,           _______,  _______, _______, _______,  _______,
+                                                                 XXXXXXX, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                                                            //`--------------'  `--------------'
+    ),
 };
 
 // Same function on all layers for now.
@@ -144,11 +155,29 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [4] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
     [5] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
     [6] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [7] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+
 };
+#else
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(MS_WHLD);
+        } else {
+            tap_code(MS_WHLU);
+        }
+    }
+    return false;
+}
 #endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
+    case _NICOLA:
+        #ifdef RGBLIGHT_ENABLE
+        rgblight_sethsv_range(HSV_PURPLE, 0, 2); // 例: NICOLA レイヤー用の色
+        #endif
+        break;
     case _LOWER:
         #ifdef RGBLIGHT_ENABLE
         rgblight_sethsv_range(HSV_BLUE, 0, 2);
@@ -196,7 +225,7 @@ static uint16_t fn_pressed_time = 0; // fn_pressed の押下時刻を保持
 // NICOLA親指シフト
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
+ switch (keycode) {
     // NICOLA親指シフト
     case NCL_OFF:
         if (record->event.pressed) {
@@ -205,6 +234,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_on(_LOWER); // レイヤー 2 をオン
         } else {
             layer_off(_LOWER); // レイヤー 2 をオフ
+
             // NCL_ON 直後（TAPPING_TERM 以内）の場合のみ、NICOLA モードと IME をオフ
             if (fn_pressed && (TIMER_DIFF_16(timer_read(), fn_pressed_time) < TAPPING_TERM)) {
                 layer_off(_NICOLA); // _NICOLA レイヤーをオフ
@@ -264,7 +294,7 @@ void matrix_scan_user(void) {
 }
 
 void matrix_init_user(void) {
-    // NICOLA親指シフト
-    set_nicola(_NICOLA);
-    // NICOLA親指シフト
+  // NICOLA親指シフト
+  set_nicola(_NICOLA);
+  // NICOLA親指シフト
 }
